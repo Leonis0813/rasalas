e7771bbb98a928ad49ce6516ad9e4cadab7246b0
Leonis0813 <Leonis.0813@gmail.com>
2019-07-17 23:11:02 +0900

    create new app

---
+++ app/src/main/java/com/leonis/android/rasalas/MainActivity.java
@@ -0,0 +1,13 @@
+package com.leonis.android.rasalas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
